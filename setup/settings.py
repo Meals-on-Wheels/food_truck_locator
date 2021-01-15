db3c8683d5902f9fe46eb581b60a882b775ecbff
Robert Radford <RobertR928@gmail.com>
2021-01-15 17:36:53 -0500

    models

--- setup/settings.py
+++ setup/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'truck.apps.TruckConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'truck')
--- truck/views.py
+++ truck/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.views import generic
+from .models import TruckInstance, Image, MenuItem
 
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html')
