ab0b687f8ecb703d84c187c890b0917a9f47fa7d
Brendan Welzien <brendanwelzien@gmail.com>
2021-01-14 16:22:20 -0800

    changes

---
+++ truck/serializers.py
@@ -0,0 +1,7 @@
+# from .models import FoodTruck
+# from rest_framework.serializers import ModelSerializer
+
+# class FoodTruckSerializer(ModelSerializer):
+    # class Meta:
+    #   model = 'insert model name here'
+    #   fields = ("id", "name", "", "") # needs to be in model and needs to match
