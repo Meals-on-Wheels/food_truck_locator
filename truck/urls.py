f0e6f4563f15406088c046ac1c2a38548b3a1435
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-01-19 16:20:07 -0800

    added stuff for google api

--- truck/urls.py
+++ truck/urls.py
@@ -4,13 +4,15 @@ from django.urls import path, re_path
 # Import all classes from the .views file
 # from .views import test_map_view
 from . import views
+from truck.google_api.google_locator import test_location
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('test', test_location),
     path('about/', views.about_view, name='about'),
     path('login/', views.login_view, name='login'),
     path('signup/', views.SignUp, name='signup'),
-    path('signed-in/', views.signIn, name="sign in")
+    path('signed-in/', views.signIn, name="sign in"),
     path('activate_account_sent/', views.activate_account_sent, name='activate_account_sent'),
     path('activate_account_invalid/', views.activate_account_invalid, name='activate_account_invalid'),
     path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate'),
