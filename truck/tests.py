79b95b533f8197f66e56ae4d3d86fec999b82aa4
Alex Peña <attn.alex.p@gmail.com>
2021-01-21 15:50:46 -0800

    added tests

--- truck/tests.py
+++ truck/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from .models import TruckInstance, MenuItem
+class TruckInstanceTestCase(TestCase):
+    def setUp(self):
+        self.menuItem = MenuItem.objects.create(item="Hummus", description="drizzle with olive oil", cost=3.99)
+    def test_menu_item_exists(self):
+        self.assertIsNotNone(self.menuItem)
